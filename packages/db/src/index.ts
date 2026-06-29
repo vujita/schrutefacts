dbe9827896b608fc4f059b4f2a664a4aa7b4a642
Vu Nguyen <shardphoneix@gmail.com>
2026-06-29 09:05:09 -0700

    Add TypeScript 7 (tsgo) for type-checking alongside TS 6

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,12 @@
 import { env } from "@schrutefacts/env/server";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-export function createDb() {
+export type Database = NodePgDatabase<typeof schema>;
+
+export function createDb(): Database {
   return drizzle(env.DATABASE_URL, { schema });
 }
 
-export const db = createDb();
+export const db: Database = createDb();
